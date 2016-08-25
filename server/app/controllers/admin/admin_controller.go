1840bba81a2bc09c4c6b950a75fcf9fe3ff7c6e7
voyageth <voyageth@gmail.com>
2016-08-25 09:58:38 +0900

    feat: Impl admin user check filter
    Related: #8

--- server/app/controllers/admin/admin_controller.go
+++ server/app/controllers/admin/admin_controller.go
@@ -10,8 +10,16 @@ type AdminController struct {
 	web_controllers.WebController
 }
 
+// TODO read from properties?
+const ADMIN_USER = "frog_login"
+
 func AdminUserCheckFilter(c *revel.Controller, fc []revel.Filter) {
-	// TODO impl
 	log.Println("AdminUserCheckFilter!")
+
+	userEmail := c.Session[web_controllers.SESSION_KEY_LOGIN]
+	if (ADMIN_USER == userEmail) {
+		c.RenderArgs["isAdminUser"] = true
+	}
+
 	fc[0](c, fc[1:])
 }
