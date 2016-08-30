16bcca0e0285f499c3c0cced37cd51ba06c885ba
voyageth <voyageth@gmail.com>
2016-08-30 09:23:50 +0900

    feat: Impl CompanyController
    - add select company list query
    
    Related: #8

--- server/app/controllers/admin/company_controller.go
+++ server/app/controllers/admin/company_controller.go
@@ -2,6 +2,8 @@ package admin_controllers
 
 import (
 	"github.com/revel/revel"
+	"github.com/voyageth/frog/server/app/models"
+	"log"
 )
 
 type CompanyController struct {
@@ -9,5 +11,14 @@ type CompanyController struct {
 }
 
 func (c CompanyController) Index() revel.Result {
+	companyList, err := c.Txn.Select(models.Company{}, "select * from Company limit 10")
+	log.Println(companyList)
+	log.Println(err)
+	if err != nil {
+		log.Println(err.Error())
+		c.Flash.Error(c.Message("server.error"))
+		c.Validation.Keep()
+		c.FlashParams()
+	}
 	return c.Render()
 }
