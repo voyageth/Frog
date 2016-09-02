cc11dc23b27690d367f461ea89339a3224032f3f
voyageth <voyageth@gmail.com>
2016-09-02 09:14:23 +0900

    feat: Impl CompanyController
    - add company register page.
    
    Related: #8

--- server/app/controllers/admin/company_controller.go
+++ server/app/controllers/admin/company_controller.go
@@ -4,6 +4,8 @@ import (
 	"github.com/revel/revel"
 	"github.com/voyageth/frog/server/app/models"
 	"log"
+	"github.com/go-sql-driver/mysql"
+	"strings"
 )
 
 type CompanyController struct {
@@ -24,5 +26,39 @@ func (c CompanyController) Index() revel.Result {
 }
 
 func (c CompanyController) Register() revel.Result {
+	return c.Render()
+}
+
+func (c CompanyController) RegisterRequest(company *models.Company) revel.Result {
+	log.Print("company : ", company)
+
+	company.Validate(c.Validation)
+
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(CompanyController.Register)
+	}
+
+	err := c.Txn.Insert(company)
+	if err != nil {
+		log.Println(err.Error())
+		if mysqlError, ok := err.(*mysql.MySQLError); ok {
+			if mysqlError.Number == 1062 {
+				if strings.Contains(err.Error(), "for key 'Name'") {
+					c.Flash.Error(c.Message("company.register.name.duplicated"))
+					c.Validation.Keep()
+					c.FlashParams()
+					return c.Redirect(CompanyController.Register)
+				}
+			}
+		}
+		c.Flash.Error(c.Message("server.error"))
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(CompanyController.Register)
+
+	}
+
 	return c.Redirect(CompanyController.Index)
 }
