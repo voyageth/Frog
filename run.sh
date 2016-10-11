f378a8ad480cd09150795fd9a0c7c91f63243d13
Sungju Kwon <sungju.kwon@campmobile.com>
2016-10-12 00:11:22 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-/home/ec2-user/go/bin/revel run github.com/voyageth/frog/server dev 8080
+export GOPATH=/home/ec2-user/go
+$GOPATH/bin/revel run github.com/voyageth/frog/server dev 8080
