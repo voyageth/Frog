67fd61430a36d12d86aa45b850002939db7cbaaf
Sungju Kwon <sungju.kwon@campmobile.com>
2016-10-11 23:55:46 +0900

    Create install_go_libs.sh

---
+++ install_go_libs.sh
@@ -0,0 +1,6 @@
+sudo yum -y install go
+go get -t github.com/revel/revel
+go get -t github.com/revel/cmd/revel
+go get -t github.com/go-gorp/gorp
+go get -t github.com/go-sql-driver/mysql
+go get -t golang.org/x/crypto/bcrypt
